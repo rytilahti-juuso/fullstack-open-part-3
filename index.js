7e61525d26b369741fd41e8ff23cb768e887b5eb
rytilahti-juuso <rytilahti-juuso@users.noreply.github.com>
2018-11-17 23:58:04 +0200

    3.3 tehty

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ app.get('/info', (req, res) => {
     `)
 })
 
+app.get('/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+    if(person){
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+
+})
+
 
 const port = 3001
 app.listen(port)
