6364b369b4bd9740ca731fbef3e4d437e04ea208
rytilahti-juuso <rytilahti-juuso@users.noreply.github.com>
2018-11-20 16:20:23 +0200

    3.7 tehty

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app= express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
-
+app.use(morgan('tiny'))
 let persons = [
   {
     name: "Arto Hellas",
