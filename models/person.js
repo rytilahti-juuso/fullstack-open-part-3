68d3e8ae4fa6f689b013463d1e322989e2a91c0d
rytilahti-juuso <rytilahti-juuso@users.noreply.github.com>
2018-12-14 23:56:33 +0200

    appi nettiin

--- models/person.js
+++ models/person.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://admin:PASSUTAHAN@ds139341.mlab.com:39341/fullstack-open-eka'
+const url = 'mongodb://admin:salasana1@ds139341.mlab.com:39341/fullstack-open-eka'
 
 mongoose.connect(url)
 
